91697eceab71f87ff0ba4ce7f18bb5e6ad1ad140
jeno8522 <68889383+jeno8522@users.noreply.github.com>
2022-09-06 22:25:20 +0900

    유기농 배추

--- 9월/1주차/DFS/Joo_1012.py
+++ 9월/1주차/DFS/Joo_1012.py
@@ -4,12 +4,12 @@ import sys
 sys.setrecursionlimit(10000)
 
 def dfs(r:int, c:int):
-    if r<0 or r>=n or c<0 or c>=m:
+    if r<0 or r>=n or c<0 or c>=m:      #리턴 조건
         return
     if graph[r][c] != 1:
         return
-    if 0<=r<n and 0<=c<m:
-        if graph[r][c] == 1:
+    if 0<=r<n and 0<=c<m:               #해당 부분에 배추가 있을 시 2로 마킹 후 dfs 재귀 호출
+        if graph[r][c] == 1:            #어차피 dfs를 초기 호출 할때 조건을 확인 하고 호출하므로 해당 부분을 제외한 상하좌우만 dfs 호출 하도록 담부터 유의
             graph[r][c] = 2
 
             dfs(r + 1, c)
@@ -27,11 +27,11 @@ for i in range(t):
     for j in range(k):
         c, r = map(int, input().split())
         graph[r][c] = 1
-        baechoo.append([r, c])
+        baechoo.append([r, c])          #배추 심은 자리를 따로 리스트에 저장
 
     while baechoo:
-        r, c = baechoo.popleft()
-        if graph[r][c] == 1:
+        r, c = baechoo.popleft()        #전체가 아닌 배추를 심은 자리만 고려
+        if graph[r][c] == 1:            #배추 있으면 dfs호출 (여기서 배추가 있는 지 유무를 확인함)
             dfs(r,c)
             cnt += 1
     # for x in range(n):

a193ef6d397ddf637dfd151534e9564e9d91e3fa
joojeno <68889383+jeno8522@users.noreply.github.com>
2023-04-05 09:14:41 +0900

    Update BOJ_G5_5639_이진검색트리.py

--- 2023(싸피 코테스터디)/3월/5주차/BOJ_G5_5639_이진검색트리.py
+++ 2023(싸피 코테스터디)/3월/5주차/BOJ_G5_5639_이진검색트리.py
@@ -1,8 +1,25 @@
 import sys
+sys.setrecursionlimit(10 ** 9)
 input = sys.stdin.readline
 
+def post(start, end):
+    if start > end:
+        return
+    mid = end + 1
+
+    for i in range(start + 1, end + 1):
+        if arr[start] < arr[i]:
+            mid = i
+            break
+    post(start + 1, mid - 1)
+    post(mid, end)
+    print(arr[start])
+
+arr = []
 while True:
-    tmp = input()
-    if tmp == '\n':
-        print("finish")
+    try:
+        arr.append(int(input()))
+    except:
         break
+
+post(0, len(arr) - 1)

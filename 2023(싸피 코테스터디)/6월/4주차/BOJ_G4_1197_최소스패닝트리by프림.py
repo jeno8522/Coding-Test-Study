bd1855242af9c7fefcb6081bce9370a7699b98a2
jeno8522 <68889383+jeno8522@users.noreply.github.com>
2023-06-23 01:05:43 +0900

    제출

--- 2023(싸피 코테스터디)/6월/4주차/BOJ_G4_1197_최소스패닝트리by프림.py
+++ 2023(싸피 코테스터디)/6월/4주차/BOJ_G4_1197_최소스패닝트리by프림.py
@@ -3,7 +3,7 @@ import heapq
 input = sys.stdin.readline
 
 
-
+#프림 알고리즘
 if __name__ == '__main__':
     V, E = map(int, input().split())
     graph = [[] for _ in range(V+1)]
@@ -14,17 +14,18 @@ if __name__ == '__main__':
         graph[e].append((v, s))
 
     hq = []
-    hq.append((0,1))
+    hq.append((0,1))    # 첫 탐색 스타트 위치 (가중치는 0, 1번 정점)
     result = 0
-    visited = [False] * (V+1)
-    visited[1] = True
+    visited = [False] * (V+1)   # 정점 방문 체크
+    # visited[1] = True
     while hq:
-        v, now = heapq.heappop(hq)
-        if not visited[now]:
-            visited[now] = True
-            result += v
-        for next in graph[now]:
-            if visited[next[1]]:
+        v, now = heapq.heappop(hq)  # 힙으로 가중치가 가장 작은 다음 정점 pop
+        if visited[now]:    # 해당 정점이 앞에서 더 작은 가중치로 이미 방문(연결) 되어 있음 -> continue
+            continue
+        visited[now] = True # 이번 정점 방문(연결) 처리, 가중치 더해줌
+        result += v
+        for next in graph[now]: # 이번 정점에 연결된 다음 정점 힙에 넣어줌
+            if visited[next[1]]:    # 다음 정점이 이미 연결되어 있다면 continue
                 continue
             heapq.heappush(hq, (next[0], next[1]))
         # print(v)
